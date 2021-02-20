34da4449e8c1c639a30904e71bac2ba68bd6faac
Johan Schobben <johanschobben@gmail.com>
2021-02-20 10:09:30 +0100

    added models

--- src/app/game/game.service.ts
+++ src/app/game/game.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+import {Game} from './models/game';
+import {Player} from './models/player';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
+  private gameSubject: BehaviorSubject<Game> = new BehaviorSubject<Game>(undefined);
 
   constructor() { }
+
+  createNewGame(players: Player[]): void {
+    const game = {
+      players
+    };
+    this.gameSubject.next(game);
+  }
 }
---
+++ src/app/game/models/game.ts
@@ -0,0 +1,5 @@
+import {Player} from './player';
+
+export interface Game {
+  players: Player[];
+}
