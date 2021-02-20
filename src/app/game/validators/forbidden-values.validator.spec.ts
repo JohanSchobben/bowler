02b6f9390dd83e1d602d128bbc155e3c1139e980
Johan Schobben <johanschobben@gmail.com>
2021-02-20 12:52:17 +0100

    add test for validator

---
+++ src/app/game/validators/forbidden-values.validator.spec.ts
@@ -0,0 +1,44 @@
+import {forbiddenValues} from './forbidden-values.validator';
+import {AbstractControl} from '@angular/forms';
+
+fdescribe('forbiddenValuesValidator', () => {
+  it('should return no error when the value is null', () => {
+    const value = null;
+    const forbiddenValuesList = [];
+    const formControl = {value} as AbstractControl;
+
+    const validationFunction = forbiddenValues(forbiddenValuesList);
+    const result = validationFunction(formControl);
+    expect(result).toBeFalsy();
+  });
+
+  it('should return no error when the forbidden values are empty', () => {
+    const value = 'John Doe';
+    const forbiddenValuesList = [];
+    const formControl = {value} as AbstractControl;
+
+    const validationFunction = forbiddenValues(forbiddenValuesList);
+    const result = validationFunction(formControl);
+    expect(result).toBeFalsy();
+  });
+
+  it('should return no error when the name is not inside the forbidden values list', () => {
+    const value = 'Jane';
+    const forbiddenValuesList = ['Steve', 'Carl', 'John'];
+    const formControl = {value} as AbstractControl;
+
+    const validationFunction = forbiddenValues(forbiddenValuesList);
+    const result = validationFunction(formControl);
+    expect(result).toBeFalsy();
+  });
+
+  it('should return an error when the name is in the forbidden values list', () => {
+    const value = 'Jane';
+    const forbiddenValuesList = ['Jane'];
+    const formControl = {value} as AbstractControl;
+
+    const validationFunction = forbiddenValues(forbiddenValuesList);
+    const result = validationFunction(formControl);
+    expect(result).toBeTruthy();
+  });
+});
