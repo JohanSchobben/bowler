f214ccfdb701099e68260246fdc8d5318ebdc902
Johan Schobben <johanschobben@gmail.com>
2021-02-21 15:27:11 +0100

    added pin service

---
+++ src/app/pin/pin.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PinComponent } from './pin.component';
+
+
+
+@NgModule({
+  declarations: [PinComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PinModule { }
