76c8d580be750bfccba1eeb7d133e7f51ae18a4e
Johan Schobben <johanschobben@gmail.com>
2021-02-22 13:19:41 +0100

    made service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {GameModule} from './game/game.module';
 import {PinModule} from './pin/pin.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,8 @@ import {PinModule} from './pin/pin.module';
   imports: [
     BrowserModule,
     GameModule,
-    PinModule
+    PinModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
